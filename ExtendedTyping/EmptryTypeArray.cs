32180db6e864de151292fcea6d436407967732a1
techClubMember <jesserussell411@gmail.com>
2020-11-29 21:21:17 -0700

    Add project files.

---
+++ ExtendedTyping/EmptryTypeArray.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ExtendedTyping
+{
+    public struct TypeArray : IEnumerable<Type>
+    {
+        public IEnumerator<Type> GetEnumerator() => Enumerable.Empty<Type>().GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
+    }
+}
---
+++ ExtendedTyping/ITyping.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExtendedTyping
+{
+    public interface ITyping
+    {
+        object O { get; }
+    }
+}
